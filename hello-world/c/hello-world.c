e7eb2c7f52207191536a334f2d4de8d9343edd26
Ebenezer R <akhonya.ebenezer@yandex.com>
2023-02-18 08:29:11 -0800

    Print "Hello World" in bash and C (#779)

--- hello-world/c/hello-world.c
+++ hello-world/c/hello-world.c
@@ -1,4 +1,17 @@
 #include<stdio.h>
-void main(){
-    printf("Hello World");
+#include "hello-world.h"
+
+/* *helloWord - print "Hello World"
+   *Description - functiont to print "Hello world to stdout"
+   *Returns: void
+*/
+
+void helloWord(void)
+{
+  printf("Hello World\n");
+}
+int main(void)
+{
+  helloWord();
+  return (0);
 }
---
+++ hello-world/shell/hello-world.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#print hello world to stdout
+
+echo -e "Hello World"

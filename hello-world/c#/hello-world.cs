88f814cd632770310cd50ee2a0236eb538d73a71
Carl Morisbak Olsson <96685368+SpookySnek@users.noreply.github.com>
2023-02-22 14:23:32 +0100

    fixed bracket formatting to convention in c# (#783)

--- hello-world/c#/hello-world.cs
+++ hello-world/c#/hello-world.cs
@@ -1,7 +1,9 @@
 using System;
 
-internal static class HelloWorld {
-  private static void Main() {
+internal static class HelloWorld 
+{
+  private static void Main() 
+  {
     Console.WriteLine("Hello World!");
   }
 }
